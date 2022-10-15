cc1096cb687f718e57257c39fe889a4239725287
KeishiroNagaseForMacBookPro <keit0728.job@gmail.com>
2022-10-16 01:14:06 +0800

    [feature] #6 解答詳細画面（モックアップ）

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import QuestionCreaterPage from './routes/question-creater-page';
 import QuestionListPage from './routes/question-list-page';
 import QuestionDescriptionPage from './routes/question-description-page';
 import AnswerListPage from './routes/answer-list-page';
+import AnswerDescriptionPage from './routes/answer-description-page';
 import NoMatch from './routes/nomatch';
 import { Routes, Route, Link} from 'react-router-dom';
 
@@ -21,6 +22,7 @@ function App() {
                 <Route path="/question-list-page" element={<QuestionListPage />} />
                 <Route path="/question-description-page" element={<QuestionDescriptionPage />} />
                 <Route path="/answer-list-page" element={<AnswerListPage />} />
+                <Route path="/Answer-description-page" element={<AnswerDescriptionPage />} />
                 <Route path="*" element={<NoMatch />} />
             </Routes>
         </div>
---
+++ src/routes/answer-description-page.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom"
+
+const AnswerDescriptionPage = () => {
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	const [title, setTitle] = useState(location.state);
+	const [description, setDescription] = useState("fuga hoge fuga hoge fuga hoge fuga hoge fuga hoge");
+	const [discordName, setDiscordName] = useState("God");
+	const [prize, setPrize] = useState("1000 yen");
+
+	const downloadAnswer = () => {
+		alert("解答をダウンロードしました！！！");
+	}
+
+	const returnCheckAnswer = () => {
+		navigate('/');
+	}
+
+	const backPage = () => {
+		navigate('/answer-list-page');
+	}
+
+	return (
+		<div className="AnswerDescriptionPage">
+			<div>
+				Title: {title.answer}
+			</div>
+			<div>
+				Description: {description}
+			</div>
+			<div>
+				Discord name: {discordName}
+			</div>
+			<div>
+				Prize: {prize}
+			</div>
+			<div>
+				<button onClick={() => downloadAnswer()}>解答をダウンロード</button>
+			</div>
+			<button onClick={() => returnCheckAnswer()}>採点結果を記入</button>
+			<button onClick={() => backPage()}>戻る</button>
+		</div>
+	);
+};
+
+export default AnswerDescriptionPage;
