58d1e5d46df0de07c2596d9be3a967815e8f9bb1
KeishiroNagaseForMacBookPro <keit0728.job@gmail.com>
2022-10-15 22:31:01 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Ludens
+        </h1>
+        <h2>
+          A Brand-New Learnfi is HERE!!!!!!
+        </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
